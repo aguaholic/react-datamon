711030fd1e59ed13eb746d156ee9487637ea52c2
JoyceOliveira <joyterra@joyceoleirasmbp.home>
2024-09-12 12:52:23 +0200

    implement service

---
+++ api/pokemon.ts
@@ -0,0 +1,43 @@
+import {
+  IPaginatedPokemonList,
+  IPokemonDetailResponse,
+  IPokemonListItem,
+  IPokemonListResponseItem,
+} from "@/types/pokemon";
+
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+export async function getAllPokemonsPaginated(
+  pageLength: number,
+  selectePage: number,
+): Promise<IPaginatedPokemonList> {
+  const offset = selectePage > 0 ? pageLength * selectePage : 0;
+
+  const pokemonListResponse = await fetch(
+    `${BASE_URL}/pokemon?limit=${pageLength}&offset=${offset}`,
+  );
+
+  const pokemonList: IPokemonListResponseItem =
+    await pokemonListResponse.json();
+
+  const pokemonDetails: IPokemonListItem[] = await Promise.all(
+    pokemonList.results.map(async (pokemonItem) => {
+      const pokemonDetailResponse = await fetch(pokemonItem.url);
+
+      const pokemonDetail: IPokemonDetailResponse =
+        await pokemonDetailResponse.json();
+
+      return {
+        id: pokemonDetail.id,
+        name: pokemonDetail.name,
+        image: pokemonDetail.sprites.front_default,
+        types: pokemonDetail.types.map((t) => t.type.name),
+      };
+    }),
+  );
+
+  return {
+    results: pokemonDetails,
+    records: pokemonList.count,
+  };
+}
